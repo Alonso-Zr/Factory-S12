6a1078d47fe77a8e881f2641129d61ec7a3dd433
Alonso-Zr <alonsoseguro27@gmail.com>
2026-06-27 23:49:35 -0500

    feat: crear clase LaptopGamer

---
+++ src/LaptopGamer.java
@@ -0,0 +1,20 @@
+public class LaptopGamer extends Laptop{
+    public LaptopGamer(String marca, String modelo, int ano_fabricacion, String tipo_procesador, Double tamano_pantalla) {
+        super(marca, modelo, ano_fabricacion, tipo_procesador, tamano_pantalla);
+    }
+
+    @Override
+    public void ejecutarPrueba() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Corriendo programas en una Laptop Gamer\n" +
+                "Laptop Gamer\n" +
+                "Marca y Modelo: " + marca + " " + modelo + "\n" +
+                "Año de Fabricacion: " + ano_fabricacion + "\n" +
+                "Procesador: " + tipo_procesador + "\n" +
+                "Tamaño de la Pantalla: " + tamano_pantalla + "\n";
+    }
+}
