d57e6fde615325ed2ac67ea0f975ef79a4dcbbf6
Alonso-Zr <alonsoseguro27@gmail.com>
2026-06-27 23:55:56 -0500

    feat: crear clase LaptopProfesional

---
+++ src/LaptopProfesional.java
@@ -0,0 +1,20 @@
+public class LaptopProfesional extends Laptop {
+    public LaptopProfesional(String marca, String modelo, int ano_fabricacion, String tipo_procesador, Double tamano_pantalla) {
+        super(marca, modelo, ano_fabricacion, tipo_procesador, tamano_pantalla);
+    }
+
+    @Override
+    public void ejecutarPrueba() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Corriendo programas en una Laptop Profesional\n" +
+                "Laptop Profesional\n" +
+                "Marca y Modelo: " + marca + " " + modelo + "\n" +
+                "Año de Fabricacion: " + ano_fabricacion + "\n" +
+                "Procesador: " + tipo_procesador + "\n" +
+                "Tamaño de la Pantalla: " + tamano_pantalla + "\n";
+    }
+}
